58798f43f569a513bf734ab91e93efd3a7853f4a
Rishabh Verma <me@rishabhverma.me>
2018-02-15 17:26:01 +0530

    Added updated config.

--- config/production.py
+++ config/production.py
@@ -1,3 +1,10 @@
 SQLALCHEMY_DATABASE_URI = 'mysql://root:Hello123!@localhost/chanakya'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 TEST_ENROLL_MSG = "Thanks for enrolling for the admission test. Your enrolment number is {enrolment_num}. You can answer the test on http://test.navgurukul.org/?enrollment_key={enrolment_num}"
+DEBUG = True
+CRM_AUTH_TOKEN = "dff429d03714ecd774b7706e358e907b"
+EXOTEL_AUTH  = {
+    'username': 'navgurukul',
+    'password': 'd8d7378e5eaa22ebf345a62dd391c479aeedb440'
+}
+EXOTEL_SMS_NUM = "01139595141"
