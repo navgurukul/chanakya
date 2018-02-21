95b6c3403ae777084b586e8288c31f2cb624b28b
Abhishek Gupta <abhishekgupta.iitd@gmail.com>
2018-02-21 17:01:29 +0000

    merging

--- migrations/env.py
+++ migrations/env.py
@@ -93,8 +93,4 @@ def run_migrations_online():
 if context.is_offline_mode():
     run_migrations_offline()
 else:
-<<<<<<< HEAD
     run_migrations_online()
-=======
-    run_migrations_online()
->>>>>>> remotes/origin/memory_test
