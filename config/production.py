a730f048b9a90cd6257f394f2583e18522b251bc
shanu khera <sha256@encrypted.directory>
2018-03-15 06:56:01 +0000

    ask form changes

--- config/production.py
+++ config/production.py
@@ -2,7 +2,7 @@ import random, string
 import os
 
 STUDENT_DIRECTORY = os.path.expanduser('/home/lawliet/student_files')
-TEST_VERSION = "Original Assessment"
+TEST_VERSION = "Rajeev's Aptitude Assessment"
 
 # Database
 #SQLALCHEMY_DATABASE_URI = 'mysql://root:hello123@35.189.19.99/chanakya'
