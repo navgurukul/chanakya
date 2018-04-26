3889b2d5b8d26c2f51b5d0a025f628b14a9580a6
shanu khera <sha256@encrypted.directory>
2018-04-26 10:22:22 +0000

    venv

--- venv/lib/python3.5/site-packages/exotel/_compat.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-try:  # pragma: no cover
-    # py3
-    from urllib.parse import urljoin
-except ImportError:  # pragma: no cover
-    # py2
-    from urlparse import urljoin
