d3dc775f5d25881e0a4dc775acb439fff38767c4
Rishabh Verma <me@rishabhverma.me>
2018-08-15 11:11:03 +0530

    changing name of the enrolment key route.

--- src/routes/start_flow.py
+++ src/routes/start_flow.py
@@ -2,7 +2,7 @@ from flask_restplus import Resource, reqparse
 from chanakya.src import api
 
 
-@api.route('/start/generate_enrolment_key')
+@api.route('/start/send_enrolment_key')
 class GenerateEnrollmentKey(Resource):
     # args_parser = reqparse.RequestParser()
     # args_parser.add_argument('PhoneNumber', type=int, help='Number to which the sms will be sent')
