24b0c56df3ed4eb5d53e05156f22f82725b3813e
amarkrsinha1997 <amar17@navgurukul.org>
2018-08-18 09:32:39 +0530

    Added the route for requested_callback

--- src/config/development.py
+++ src/config/development.py
@@ -4,3 +4,9 @@ class DevelopmentConfig(ChanakyaConfig):
     DEBUG=True
     SQLALCHEMY_DATABASE_URI = "mysql://root:root@localhost/chanakya"
     TEST_DURATION = 3640 # In seconds
+
+
+    STAGES = {
+        'RQC': 'REQUESTED CALLBACK',
+        'EKG': 'ENROLLMENT KEY GENERATED'
+    }
