24ade08125c975b317c4dbbf8d49d89c731ff632
pralhad88 <pralhad18@navgurukul.org>
2019-12-06 19:51:29 +0530

    lint solved

--- server/index.js
+++ server/index.js
@@ -36,10 +36,10 @@ exports.deployment = async (start) => {
     });
 
     // Inform students test result sending to SMS after 4 hours.
-    // cron.schedule(CONSTANTS.testResultCron, () =>{
-    //     const { assessmentService } = server.services();
-    //     assessmentService.informTestResult();
-    // });
+    cron.schedule(CONSTANTS.testResultCron, () =>{
+        const { assessmentService } = server.services();
+        assessmentService.informTestResult();
+    });
 
     return server;
 };
