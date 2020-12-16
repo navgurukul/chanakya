bf8b9dd5a4bc950d3a2a9f67b158cdf200986d56
pysaquib <iamsaquibnasim@gmail.com>
2020-12-16 21:06:37 +0530

    Cron after every 2 mins to clear knex unused connections

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const Glue = require("@hapi/glue");
 const Manifest = require("./manifest");
 const cron = require("node-cron");
 const CONSTANTS = require("../lib/constants");
+const knex = require("knex");
 // taking mode of node environment from .env file.
 const Dotenv = require("dotenv");
 Dotenv.config({ path: `${__dirname}/../.env` });
@@ -41,6 +42,40 @@ exports.deployment = async (start) => {
   //     const { metricsService } = server.services();
   //     metricsService.recordPendingMetrics();
   // });
+
+  cron.schedule(`2 * * * * *`, () => {
+    knex.raw(`WITH inactive_connections AS (
+      SELECT
+          pid,
+          rank() over (partition by client_addr order by backend_start ASC) as rank
+      FROM
+          pg_stat_activity
+      WHERE
+          -- Exclude the thread owned connection (ie no auto-kill)
+          pid <> pg_backend_pid( )
+      AND
+          -- Exclude known applications connections
+          application_name !~ '(?:psql)|(?:pgAdmin.+)'
+      AND
+          -- Include connections to the same database the thread is connected to
+          datname = current_database()
+      AND
+          -- Include connections using the same thread username connection
+          usename = current_user
+      AND
+          -- Include inactive connections only
+          state in ('idle', 'idle in transaction', 'idle in transaction (aborted)', 'disabled')
+      AND
+          -- Include old connections (found with the state_change field)
+          current_timestamp - state_change > interval '5 minutes'
+  )
+  SELECT
+      pg_terminate_backend(pid)
+  FROM
+      inactive_connections
+  WHERE
+      rank > 1 -- Leave one connection for each application connected to the database`);
+  });
 
   // Inform pending mobilization work to user sending to SMS after 1 hours.
   cron.schedule(CONSTANTS.deadlineResultCron, () => {
--- server/manifest.js
+++ server/manifest.js
@@ -1,140 +1,148 @@
-'use strict';
+"use strict";
 
-const Dotenv = require('dotenv');
-const Confidence = require('confidence');
-const Toys = require('toys');
-const Boom = require('boom');
+const Dotenv = require("dotenv");
+const Confidence = require("confidence");
+const Toys = require("toys");
+const Boom = require("boom");
 
 // Pull .env into process.env
 Dotenv.config({ path: `${__dirname}/../.env` });
 
 // Glue manifest as a confidence store
 module.exports = new Confidence.Store({
-    server: {
-        host: 'localhost',
-        port: {
-            $filter: { $env: 'NODE_ENV' },
-            $default: {
-                $env: 'PORT',
-                $coerce: 'number',
-                $default: 3000
-            },
-            test: { $value: undefined }         // Let the server find an open port
+  server: {
+    host: "localhost",
+    port: {
+      $filter: { $env: "NODE_ENV" },
+      $default: {
+        $env: "PORT",
+        $coerce: "number",
+        $default: 3000,
+      },
+      test: { $value: undefined }, // Let the server find an open port
+    },
+    routes: {
+      cors: {
+        origin: ["*"],
+        additionalHeaders: ["cache-control", "x-requested-with"],
+        headers: [
+          "Accept",
+          "Authorization",
+          "Content-Type",
+          "If-None-Match",
+          "Accept-language",
+        ],
+      },
+      timeout: {
+        socket: 11 * 60 * 1000, // Determines how long before closing request socket.
+        server: false, // Determines how long to wait for server request processing. Disabled by default
+      },
+      validate: {
+        options: {
+          abortEarly: false,
         },
-        routes: {
-            cors: {
-                origin: ['*'],
-                additionalHeaders: ['cache-control', 'x-requested-with'],
-                headers: ['Accept', 'Authorization', 'Content-Type', 'If-None-Match', 'Accept-language']
-            },
-            timeout: {
-                socket: 11 * 60 * 1000, // Determines how long before closing request socket.
-                server: false // Determines how long to wait for server request processing. Disabled by default
-            },
-            validate: {
-              options: {
-                abortEarly: false,
-              },
-              failAction: async (request, h, err) => {
-                if (process.env.NODE_ENV === 'production') {
-                  // In prod, log a limited error message and throw the default Bad Request error.
-                  console.error('ValidationError:', err.message);
-                  throw Boom.badRequest(`Invalid request payload input`);
-                } else {
-                  // During development, log and respond with the full error.
-                  console.error(err);
-                  throw err;
-                }
-              }
-            }
+        failAction: async (request, h, err) => {
+          if (process.env.NODE_ENV === "production") {
+            // In prod, log a limited error message and throw the default Bad Request error.
+            console.error("ValidationError:", err.message);
+            throw Boom.badRequest(`Invalid request payload input`);
+          } else {
+            // During development, log and respond with the full error.
+            console.error(err);
+            throw err;
+          }
         },
-        debug: {
-            $filter: { $env: 'NODE_ENV' },
+      },
+    },
+    debug: {
+      $filter: { $env: "NODE_ENV" },
+      $default: {
+        log: "*",
+        request: "*",
+      },
+      production: {
+        log: ["error"],
+        request: ["error"],
+      },
+    },
+  },
+  register: {
+    plugins: [
+      {
+        plugin: "../lib", // Main plugin
+        options: {
+          jwtKey: {
+            $filter: { $env: "NODE_ENV" },
             $default: {
-                log: '*',
-                request: '*'
+              $env: "APP_SECRET",
+              $default: "app-secret",
             },
             production: {
-                log: ['error'],
-                request: ['error']
-            }
-        }
-    },
-    register: {
-        plugins: [
-            {
-                plugin: '../lib', // Main plugin
-                options: {
-                    jwtKey: {
-                        $filter: { $env: 'NODE_ENV' },
-                        $default: {
-                            $env: 'APP_SECRET',
-                            $default: 'app-secret'
-                        },
-                        production: {           // In production do not default to "app-secret"
-                            $env: 'APP_SECRET'
-                        }
-                    }
-                }
+              // In production do not default to "app-secret"
+              $env: "APP_SECRET",
             },
-            {
-                plugin: {
-                    $filter: { $env: 'NODE_ENV' },
-                    $default: 'hpal-debug',
-                    production: Toys.noop
-                }
+          },
+        },
+      },
+      {
+        plugin: {
+          $filter: { $env: "NODE_ENV" },
+          $default: "hpal-debug",
+          production: Toys.noop,
+        },
+      },
+      {
+        plugin: "schwifty",
+        options: {
+          $filter: { $env: "NODE_ENV" },
+          $default: {},
+          $base: {
+            migrateOnStart: true,
+            knex: {
+              client: "pg",
+              migrations: {
+                stub: "lib/migrations/templates/defaultMigrationTemplate.js",
+              },
+              connection: {
+                database: {
+                  $env: "DB_NAME",
+                },
+                host: {
+                  $env: "DB_HOST",
+                },
+                user: {
+                  $env: "DB_USER",
+                },
+                password: {
+                  $env: "DB_PASS",
+                },
+                requestTimeout: 90000,
+                connectionTimeout: 30000,
+                acquireConnectionTimeout: 30000,
+                typeCast: function (field, next) {
+                  // Convert 1 to true, 0 to false, and leave null alone
+                  if (field.type === "TINY" && field.length === 1) {
+                    const value = field.string();
+                    return value ? value === "1" : null;
+                  }
+                  return next();
+                },
+              },
+              pool: {
+                min: 4,
+                max: 50,
+                propagateCreateError: false,
+              },
             },
-            {
-                plugin: 'schwifty',
-                options: {
-                    $filter: { $env: 'NODE_ENV' },
-                    $default: {},
-                    $base: {
-                        migrateOnStart: true,
-                        knex: {
-                            client: "pg",
-                            migrations: {
-                                stub: 'lib/migrations/templates/defaultMigrationTemplate.js'
-                            },
-                            connection: {
-                                database: {
-                                    $env: 'DB_NAME',
-                                },
-                                host: {
-                                    $env: 'DB_HOST'
-                                },
-                                user: {
-                                    $env: 'DB_USER'
-                                },
-                                password: {
-                                    $env: 'DB_PASS'
-                                },
-                                requestTimeout: 90000,
-                                connectionTimeout: 30000,
-                                acquireConnectionTimeout: 30000,
-                                typeCast: function(field, next) {
-                                // Convert 1 to true, 0 to false, and leave null alone
-                                if (field.type === 'TINY' && field.length === 1) {
-                                    const value = field.string()
-                                    return value ? value === '1' : null
-                                }
-                                return next()
-                                }
-                            },
-                            pool: {
-                                min: 4,
-                                max: 10
-                            }
-                        }
-                    },
-                    production: {
-                        migrateOnStart: false
-                    }
-                }
-            },
-            {
-                plugin: './plugins/swagger'
-            },
-        ]
-    }
+          },
+          production: {
+            migrateOnStart: false,
+          },
+        },
+      },
+      {
+        plugin: "./plugins/swagger",
+      },
+    ],
+  },
 });
