0ec949ddd89a3e45dfa929e89826ed494482f238
pysaquib <iamsaquibnasim@gmail.com>
2021-03-31 14:42:58 +0530

    partners districts null error fixed in test link

--- webapp/static/script.js
+++ webapp/static/script.js
@@ -145,7 +145,7 @@ function fetchPartnersDistricts() {
     (data) => {
       console.log(data.data);
       var select = document.getElementById("city_or_village");
-      if (data.data.partner[0] !== undefined) {
+      if (data.data.partner[0] !== undefined || data.data.partner[0] !== null) {
         data.data.partner[0].districts.map((district) => {
           var opt = document.createElement("option");
           opt.value = district;
