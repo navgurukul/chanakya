1db0e9924eca7c73980241a1670aa25e00dc37d6
Vaibhav Magar <vaibhavmagar1901@gmail.com>
2021-12-08 15:33:57 +0530

    logic to choose and display profile photo

--- webapp/static/script.js
+++ webapp/static/script.js
@@ -188,6 +188,45 @@ function fetchState() {
 }
 
 function getInfo() {
+  //declearing html elements
+
+  const imgDiv = document.querySelector('.profile-pic-div');
+  const img = document.querySelector('#photo');
+  const file = document.querySelector('#file');
+  const uploadBtn = document.querySelector('#uploadBtn');
+
+  //if user hover on img div 
+
+  imgDiv.addEventListener('mouseenter', function(){
+      uploadBtn.style.display = "block";
+  });
+
+  //if we hover out from img div
+
+  imgDiv.addEventListener('mouseleave', function(){
+      uploadBtn.style.display = "none";
+  });
+
+  //image showing functionality when we choose an image to upload
+
+  //when we choose a photo to upload
+
+  file.addEventListener('change', function(){
+      //this refers to file
+      const choosedFile = this.files[0];
+
+      if (choosedFile) {
+
+          const reader = new FileReader(); //FileReader is a predefined function of JS
+
+          reader.addEventListener('load', function(){
+              img.setAttribute('src', reader.result);
+          });
+
+          reader.readAsDataURL(choosedFile);
+      }
+  });
+
   var url_string = window.location.href; 
   var url = new URL(url_string);
 
