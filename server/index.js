76e0fabd7678e05b92db3d7465460bd6782d3cb6
majjikishore007 <majjikishore031@gmail.com>
2021-12-29 23:25:44 +0530

    implemented the redflags task -updated

--- server/index.js
+++ server/index.js
@@ -150,11 +150,7 @@ exports.deployment = async (start) => {
   });
 
   // email sedhuler for partners
-  const { studentService } = server.services();
-  cron.schedule("0 9 * * *", () => {
-    studentService.checkForRedflag();
-  });
-  
+
   return server;
 };
 
