1094cba535ec8eb243c3054acdf515f561398bab
Poonam-Singh-Bagh <poonambagh98@gmail.com>
2023-11-17 17:31:32 +0530

    created content get api

--- server/manifest.js
+++ server/manifest.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
-const Dotenv = require("dotenv");
-const Confidence = require("confidence");
-const Toys = require("toys");
-const Boom = require("boom");
+const Dotenv = require('dotenv');
+const Confidence = require('confidence');
+const Toys = require('toys');
+const Boom = require('boom');
 
 // Pull .env into process.env
 Dotenv.config({ path: `${__dirname}/../.env` });
@@ -11,32 +11,21 @@ Dotenv.config({ path: `${__dirname}/../.env` });
 // Glue manifest as a confidence store
 module.exports = new Confidence.Store({
   server: {
-    host: "localhost",
+    host: 'localhost',
     port: {
-      $filter: { $env: "NODE_ENV" },
+      $filter: { $env: 'NODE_ENV' },
       $default: {
-        $env: "PORT",
-        $coerce: "number",
+        $env: 'PORT',
+        $coerce: 'number',
         $default: 3000,
       },
       test: { $value: undefined }, // Let the server find an open port
     },
     routes: {
       cors: {
-        origin: ["*"],
-        additionalHeaders: [
-          "cache-control",
-          "x-requested-with",
-          "platform",
-          "version-code",
-        ],
-        headers: [
-          "Accept",
-          "Authorization",
-          "Content-Type",
-          "If-None-Match",
-          "Accept-language",
-        ],
+        origin: ['*'],
+        additionalHeaders: ['cache-control', 'x-requested-with', 'platform', 'version-code'],
+        headers: ['Accept', 'Authorization', 'Content-Type', 'If-None-Match', 'Accept-language'],
       },
       timeout: {
         socket: 11 * 60 * 1000, // Determines how long before closing request socket.
@@ -47,9 +36,9 @@ module.exports = new Confidence.Store({
           abortEarly: false,
         },
         failAction: async (request, h, err) => {
-          if (process.env.NODE_ENV === "production") {
+          if (process.env.NODE_ENV === 'production') {
             // In prod, log a limited error message and throw the default Bad Request error.
-            console.error("ValidationError:", err.message);
+            console.error('ValidationError:', err.message);
             throw Boom.badRequest(`Invalid request payload input`);
           } else {
             // During development, log and respond with the full error.
@@ -60,75 +49,75 @@ module.exports = new Confidence.Store({
       },
     },
     debug: {
-      $filter: { $env: "NODE_ENV" },
+      $filter: { $env: 'NODE_ENV' },
       $default: {
-        log: "*",
-        request: "*",
+        log: '*',
+        request: '*',
       },
       production: {
-        log: ["error"],
-        request: ["error"],
+        log: ['error'],
+        request: ['error'],
       },
     },
   },
   register: {
     plugins: [
       {
-        plugin: "../lib", // Main plugin
+        plugin: '../lib', // Main plugin
         options: {
           jwtKey: {
-            $filter: { $env: "NODE_ENV" },
+            $filter: { $env: 'NODE_ENV' },
             $default: {
-              $env: "APP_SECRET",
-              $default: "app-secret",
+              $env: 'APP_SECRET',
+              $default: 'app-secret',
             },
             production: {
               // In production do not default to "app-secret"
-              $env: "APP_SECRET",
+              $env: 'APP_SECRET',
             },
           },
         },
       },
       {
         plugin: {
-          $filter: { $env: "NODE_ENV" },
-          $default: "hpal-debug",
+          $filter: { $env: 'NODE_ENV' },
+          $default: 'hpal-debug',
           production: Toys.noop,
         },
       },
       {
-        plugin: "schwifty",
+        plugin: 'schwifty',
         options: {
-          $filter: { $env: "NODE_ENV" },
+          $filter: { $env: 'NODE_ENV' },
           $default: {},
           $base: {
             migrateOnStart: true,
             knex: {
-              client: "pg",
+              client: 'pg',
               migrations: {
-                stub: "lib/migrations/templates/defaultMigrationTemplate.js",
+                stub: 'lib/migrations/templates/defaultMigrationTemplate.js',
               },
               connection: {
                 database: {
-                  $env: "DB_NAME",
+                  $env: 'DB_NAME',
                 },
                 host: {
-                  $env: "DB_HOST",
+                  $env: 'DB_HOST',
                 },
                 user: {
-                  $env: "DB_USER",
+                  $env: 'DB_USER',
                 },
                 password: {
-                  $env: "DB_PASS",
+                  $env: 'DB_PASS',
                 },
                 requestTimeout: 90000,
                 connectionTimeout: 30000,
                 acquireConnectionTimeout: 30000,
                 typeCast: function (field, next) {
                   // Convert 1 to true, 0 to false, and leave null alone
-                  if (field.type === "TINY" && field.length === 1) {
+                  if (field.type === 'TINY' && field.length === 1) {
                     const value = field.string();
-                    return value ? value === "1" : null;
+                    return value ? value === '1' : null;
                   }
                   return next();
                 },
@@ -146,24 +135,23 @@ module.exports = new Confidence.Store({
         },
       },
       {
-        plugin: "./plugins/swagger",
+        plugin: './plugins/swagger',
       },
-      {
-        // eslint-disable-next-line
-        plugin: require('hapi-sentry'),
-        options: {
-          client: {
-            dsn: {
-              $env: 'DSN',
-            },
-            // Set tracesSampleRate to 1.0 to capture 100%
-            // of transactions for performance monitoring.
-            // We recommend adjusting this value in production
-            tracesSampleRate: 1.0,
-          },
-        },
-      },
-
+      // {
+      //   // eslint-disable-next-line
+      //   plugin: require('hapi-sentry'),
+      //   options: {
+      //     client: {
+      //       dsn: {
+      //         $env: 'DSN',
+      //       },
+      //       // Set tracesSampleRate to 1.0 to capture 100%
+      //       // of transactions for performance monitoring.
+      //       // We recommend adjusting this value in production
+      //       tracesSampleRate: 1.0,
+      //     },
+      //   },
+      // },
     ],
   },
 });
